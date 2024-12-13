5fea56b91e1126e306c31680ba987a80f5f6c90d
wisomar <wisomar@gmail.com>
2024-12-13 17:42:30 -0300

    alterações script3

--- api/index.py
+++ api/index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Oi Meu Nome é William!</p>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Ponto de entrada para o Vercel
+def handler(environ, start_response):
+    from werkzeug.wrappers import Request, Response
+    request = Request(environ)
+    response = app.dispatch_request()
+    return response(environ, start_response)
